492b36b87dc267587318475f1f42eee32ade71b4
kimdohun0104 <dikolight203@gmail.com>
2020-07-28 19:38:38 +0900

    Delete useless projects

--- olive_you/android/app/src/main/kotlin/com/example/olive_you/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.olive_you
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ rotating_drawer/lib/main.dart
@@ -0,0 +1,113 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: CustomDrawer(),
+    );
+  }
+}
+
+class CustomDrawer extends StatefulWidget {
+  @override
+  _CustomDrawerState createState() => _CustomDrawerState();
+}
+
+class _CustomDrawerState extends State<CustomDrawer>
+    with SingleTickerProviderStateMixin {
+  static const double _maxSlide = 220.0;
+  static const double _minDragStartEdge = 60;
+  static const double _maxDratStartEdge = _maxSlide - 16;
+
+  AnimationController _animationController;
+  bool _canBeDragged;
+
+  @override
+  void initState() {
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 250),
+    );
+    _canBeDragged = true;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onHorizontalDragStart: _onDragStart,
+      onHorizontalDragUpdate: _onDragUpdate,
+      onHorizontalDragEnd: _onDragEnd,
+      onTap: toggle,
+      child: AnimatedBuilder(
+        animation: _animationController,
+        builder: (context, _) {
+          double slide = _maxSlide * _animationController.value;
+          double scale = 1 - (_animationController.value * 0.3);
+          return Stack(
+            children: [
+              Container(color: Colors.blue),
+              Transform.translate(
+                offset: Offset(_maxSlide * _animationController.value, 0),
+                child: Transform(
+                    child: Container(color: Colors.yellow),
+                    alignment: Alignment.centerLeft,
+                    transform: Matrix4.identity()
+                      ..setEntry(3, 2, 0.01)
+                      ..rotateY(pi / 2 * _animationController.value)),
+              ),
+            ],
+          );
+        },
+      ),
+    );
+  }
+
+  void toggle() => _animationController.isDismissed
+      ? _animationController.forward()
+      : _animationController.reverse();
+
+  void _onDragStart(DragStartDetails details) {
+    bool isDragOpenFromLeft = _animationController.isDismissed &&
+        details.globalPosition.dx < _minDragStartEdge;
+    bool isDragCloseFromRight = _animationController.isCompleted &&
+        details.globalPosition.dx > _maxDratStartEdge;
+
+    _canBeDragged = isDragOpenFromLeft || isDragCloseFromRight;
+  }
+
+  void _onDragUpdate(DragUpdateDetails details) {
+    if (_canBeDragged) {
+      double delta = details.primaryDelta / _maxSlide;
+      _animationController.value += delta;
+    }
+  }
+
+  void _onDragEnd(DragEndDetails details) {
+    if (_animationController.isDismissed || _animationController.isCompleted) {
+      return;
+    }
+    if (details.velocity.pixelsPerSecond.dx.abs() >= 365) {
+      double visualVelocity = details.velocity.pixelsPerSecond.dx /
+          MediaQuery.of(context).size.width;
+
+      _animationController.fling(velocity: visualVelocity);
+    } else if (_animationController.value < 0.5) {
+      _animationController.forward();
+    } else {
+      _animationController.reverse();
+    }
+  }
+}
